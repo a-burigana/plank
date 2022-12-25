55224c0c0cda89a8de7ffd49431360b0a1a3cd5c
Alessandro.Burigana <burigana@inf.unibz.it>
2022-12-25 23:10:50 +0100

    Small change

--- src/lex.h
+++ src/lex.h
@@ -177,3 +177,138 @@ class lexer {
 };
 
 #endif  // EPDDL_LEX_H_
+
+//class token_type {
+//public:
+//    static const uint8_t eof     = 0;
+//    static const uint8_t invalid = 1;
+//
+//    enum class punctuation : uint8_t {
+//        lpar,                  // (
+//        rpar,                  // )
+//        lbrack,                // [
+//        rbrack,                // ]
+//        lbrace,                // {
+//        rbrace,                // }
+//        lt,                    // <
+//        gt,                    // >
+//        dash,                  // -
+//        eq,                    // =
+////        colon,                 // :
+//        gets,                  // <-
+//    };
+//
+//    enum class basic : uint8_t {
+//        ident,                 // [_a-zA-Z][_'a-zA-Z0-9]*
+//        var,                   // \?[_a-zA-Z][_'a-zA-Z0-9]*
+//        modality,              // [a-z][_a-zA-Z0-9]*\.
+//        integer,               // [0-9]|[1-9][0-9]+
+//    };
+//
+//    enum class type : uint8_t {
+//        agent,                 // agent
+//        predicate,             // predicate
+//        literal,               // literal
+//        predicate_formula,     // predicate-formula
+//        formula,               // formula
+//        postcondition          // postcondition
+//    };
+//
+//    enum class keyword : uint8_t {
+//        // Main keywords
+//        _EPDDL_DEF,             // define
+//        _EPDDL_DOM,             // domain
+//        _EPDDL_LIB,             // library
+//        _EPDDL_PROB,            // problem
+//
+//        // Block keywords
+//        _BLOCK_ACT,             // :action
+//        _BLOCK_ACT_TYPE,        // :action-type
+//        _BLOCK_ACT_TYPE_LIB,    // :action-type-libraries
+//        _BLOCK_AGENTS,          // :agents
+//        _BLOCK_AGENT_GROUPS,    // :agent-groups
+//        _BLOCK_DESIGNATED,      // :designated
+//        _BLOCK_DOMAIN,          // :domain
+//        _BLOCK_EVENT,           // :event
+//        _BLOCK_EVENTS,          // :events
+//        _BLOCK_GOAL,            // :goal
+//        _BLOCK_INIT,            // :init
+//        _BLOCK_OBJECTS,         // :objects
+//        _BLOCK_OBS_COND,        // :observability-conditions
+//        _BLOCK_OBS_GROUP,       // :observability-groups
+//        _BLOCK_MODALITIES,      // :modalities
+//        _BLOCK_PARAM,           // :parameters
+//        _BLOCK_POSTCOND,        // :postconditions
+//        _BLOCK_PRECOND,         // :precondition
+//        _BLOCK_PREDICATES,      // :predicates
+//        _BLOCK_REL,             // :relations
+//        _BLOCK_REQ,             // :requirements
+//        _BLOCK_STATE,           // :state
+//        _BLOCK_STATE_NAME,      // :state-name
+//        _BLOCK_STATIC,          // :static
+//        _BLOCK_TYPES,           // :types
+//        _BLOCK_VAL,             // :valuation
+//        _BLOCK_WHERE,           // :where
+//        _BLOCK_WORLDS,          // :worlds
+//
+//        // Agent groups keywords
+//        _AG_GROUP_ALL,          // All
+//        _AG_GROUP_AS,           // as
+//    };
+//
+//    enum class requirement : uint8_t {
+//        _REQ_CK,                // :common-knowledge
+//        _REQ_DEL,               // :del
+//        _REQ_DYN_CK,            // :dynamic-common-knowledge
+//        _REQ_EQ,                // :equality
+//        _REQ_EXIST_FORM,        // :existential-formulae
+//        _REQ_FIN_S5_TH,         // :finitary-S5-theory
+//        _REQ_MAX_DEPTH,         // :maximum-modal-depth
+//        _REQ_MAX_PRE_DEPTH,     // :maximum-postconditions-depth
+//        _REQ_MAX_POST_DEPTH,    // :maximum-preconditions-depth
+//        _REQ_MOD_POST,          // :modal-postconditions
+//        _REQ_MOD_PRE,           // :modal-preconditions
+//        _REQ_ONTIC,             // :ontic-change
+//        _REQ_TYPING,            // :typing
+//        _REQ_UNIV_FORM,         // :universal-formulae
+//        _REQ_UNIV_POST          // :universal-postconditions
+//    };
+//
+//    class formula {
+//    public:
+//        class connective {
+//            enum class unary : uint8_t {
+//                negation               // not
+//            };
+//
+//            enum class binary : uint8_t {
+//                implication,           // imply
+//            };
+//
+//            enum class n_ary : uint8_t {
+//                conjunction,           // and
+//                disjunction            // or
+//            };
+//        };
+//
+//        enum class quantifier : uint8_t {
+//            exists,           // exists
+//            forall,           // forall
+//        };
+//
+//        enum class constant : uint8_t {
+//            top,             // true
+//            bot              // false
+//        };
+//    };
+//
+//    enum class postcondition : uint8_t {
+//        iff,              // iff
+//        when              // when
+//    };
+//
+//    enum class observability : uint8_t {
+//        if_,               // if
+//        otherwise          // otherwise
+//    };
+//};
