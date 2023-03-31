9d3b8652e11ad5c818504a7af478e688feef08d0
Alessandro.Burigana <burigana@inf.unibz.it>
2023-03-31 14:21:01 +0200

    Small change

--- include/grammar/token_enums.h
+++ include/grammar/token_enums.h
@@ -1,17 +1,17 @@
 #ifndef EPDDL_TOKEN_ENUMS_H
 #define EPDDL_TOKEN_ENUMS_H
 
-#define SPECIAL_ENUM         ENUM_NAME(special_value)
-#define PUNCTUATION_ENUM     ENUM_NAME(punctuation_value)
-#define BASIC_ENUM           ENUM_NAME(basic_value)
-#define KEYWORD_ENUM         ENUM_NAME(keyword_value)
-#define CONNECTIVE_ENUM      ENUM_NAME(connective_value)
-#define QUANTIFIER_ENUM      ENUM_NAME(quantifier_value)
-#define ATOMIC_FORMULA_ENUM  ENUM_NAME(atomic_formula_value)
-#define POSTCONDITION_ENUM   ENUM_NAME(postcondition_value)
-#define OBSERVABILITY_ENUM   ENUM_NAME(observability_value)
-#define AGENT_GROUP_ENUM     ENUM_NAME(agent_group_value)
-#define RESERVED_TYPE_ENUM   ENUM_NAME(reserved_type_value)
+#define SPECIAL_ENUM        ENUM_NAME(special_value)
+#define PUNCTUATION_ENUM    ENUM_NAME(punctuation_value)
+#define BASIC_ENUM          ENUM_NAME(basic_value)
+#define KEYWORD_ENUM        ENUM_NAME(keyword_value)
+#define CONNECTIVE_ENUM     ENUM_NAME(connective_value)
+#define QUANTIFIER_ENUM     ENUM_NAME(quantifier_value)
+#define ATOMIC_FORMULA_ENUM ENUM_NAME(atomic_formula_value)
+#define POSTCONDITION_ENUM  ENUM_NAME(postcondition_value)
+#define OBSERVABILITY_ENUM  ENUM_NAME(observability_value)
+#define AGENT_GROUP_ENUM    ENUM_NAME(agent_group_value)
+#define RESERVED_TYPE_ENUM  ENUM_NAME(reserved_type_value)
 
 #define ALL_ENUMS \
     SPECIAL_ENUM \
