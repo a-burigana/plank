14cb10c68f8367d05e761addaf04bd14162fff3b
Alessandro.Burigana <burigana@inf.unibz.it>
2023-04-19 11:49:55 +0200

    Added definitions for propositional formulae

--- include/grammar/formulae/propositional_formula_def.h
+++ include/grammar/formulae/propositional_formula_def.h
@@ -4,10 +4,16 @@
 #include "formula_def.h"
 #include "../tokens/tokens_def.h"
 
-#define epddl_not_formula not_formula
+#define epddl_not_formula   not_formula
+#define epddl_and_formula   and_formula
+#define epddl_or_formula    or_formula
+#define epddl_imply_formula imply_formula
 
 #define epddl_all_propositional_formula_element_defs \
-//    epddl_not_formula_def
+    epddl_not_formula_def                            \
+    epddl_and_formula_def                            \
+    epddl_or_formula_def                             \
+    epddl_imply_formula_def
 
 #define epddl_all_propositional_formula_auxiliary_type_defs
 
@@ -25,6 +31,56 @@
             node(epddl_formula)            \
             terminal(epddl_tok_rpar)       \
         )                                  \
+    )
+
+#define epddl_and_formula_def \
+    epddl_element(                            \
+        ast_class(                            \
+            class_name(epddl_and_formula),    \
+            class_params(                     \
+                list_param(epddl_formula, fs) \
+            )                                 \
+        ),                                    \
+        parse_element(                        \
+            terminal(epddl_tok_lpar)          \
+            terminal(epddl_tok_conjunction)   \
+            list_node(epddl_formula)          \
+            terminal(epddl_tok_rpar)          \
+        )                                     \
+    )
+
+#define epddl_or_formula_def \
+    epddl_element(                            \
+        ast_class(                            \
+            class_name(epddl_or_formula),     \
+            class_params(                     \
+                list_param(epddl_formula, fs) \
+            )                                 \
+        ),                                    \
+        parse_element(                        \
+            terminal(epddl_tok_lpar)          \
+            terminal(epddl_tok_disjunction)   \
+            list_node(epddl_formula)          \
+            terminal(epddl_tok_rpar)          \
+        )                                     \
+    )
+
+#define epddl_imply_formula_def \
+    epddl_element(                           \
+        ast_class(                           \
+            class_name(epddl_imply_formula), \
+            class_params(                    \
+                param(epddl_formula, f1),    \
+                param(epddl_formula, f2)     \
+            )                                \
+        ),                                   \
+        parse_element(                       \
+            terminal(epddl_tok_lpar)         \
+            terminal(epddl_tok_disjunction)  \
+            node(epddl_formula)              \
+            node(epddl_formula)              \
+            terminal(epddl_tok_rpar)         \
+        )                                    \
     )
 
 #endif //EPDDL_PROPOSITIONAL_FORMULA_DEF_H
