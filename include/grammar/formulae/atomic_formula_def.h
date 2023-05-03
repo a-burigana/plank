c5a2f6e478a01a3a12888b45ac68abb8dbe3cd43
Alessandro.Burigana <burigana@inf.unibz.it>
2023-05-03 15:32:48 +0200

    Defined true/false formulae

--- include/grammar/formulae/atomic_formula_def.h
+++ include/grammar/formulae/atomic_formula_def.h
@@ -1,15 +1,19 @@
 #ifndef EPDDL_ATOMIC_FORMULA_DEF_H
 #define EPDDL_ATOMIC_FORMULA_DEF_H
 
-#define epddl_atomic_formula    atomic_formula
-#define epddl_term              term
-#define epddl_predicate         predicate
-#define epddl_negated_predicate negated_predicate
-#define epddl_literal           literal
-#define epddl_formula_variable  formula_variable
-#define epddl_eq_formula        eq_formula
+#define epddl_epddl_true_formula  epddl_true_formula
+#define epddl_epddl_false_formula epddl_false_formula
+#define epddl_atomic_formula      atomic_formula
+#define epddl_term                term
+#define epddl_predicate           predicate
+#define epddl_negated_predicate   negated_predicate
+#define epddl_literal             literal
+#define epddl_formula_variable    formula_variable
+#define epddl_eq_formula          eq_formula
 
 #define epddl_all_atomic_formula_element_defs \
+    epddl_true_formula_def                    \
+    epddl_false_formula_def                   \
     epddl_predicate_def                       \
     epddl_negated_predicate_def               \
     epddl_formula_variable_def                \
@@ -20,14 +24,40 @@
     epddl_atomic_formula_def                         \
     epddl_literal_def
 
+#define epddl_true_formula_def \
+    epddl_element(                                  \
+        ast_class(                                  \
+            class_name(epddl_true_formula),         \
+            class_params()                          \
+        ),                                          \
+        parse_element(                              \
+            terminal(epddl_tok_lpar)                \
+            terminal(epddl_tok_top)                 \
+            terminal(epddl_tok_rpar)                \
+        )                                           \
+    )
+
+#define epddl_false_formula_def \
+    epddl_element(                                  \
+        ast_class(                                  \
+            class_name(epddl_false_formula),        \
+            class_params()                          \
+        ),                                          \
+        parse_element(                              \
+            terminal(epddl_tok_lpar)                \
+            terminal(epddl_tok_bot)                 \
+            terminal(epddl_tok_rpar)                \
+        )                                           \
+    )
+
 #define epddl_atomic_formula_def \
     epddl_auxiliary_type(                       \
         type_name(epddl_atomic_formula),        \
         variant(                                \
             ast_class(epddl_predicate),         \
             ast_class(epddl_formula_variable),  \
-            epddl_tok_bot,                      \
-            epddl_tok_top                       \
+            ast_class(epddl_true_formula),      \
+            ast_class(epddl_false_formula)      \
         )                                       \
     )
 
