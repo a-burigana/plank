04b8520da01dc9f437aa4cac84a7f48a91603ad2
Alessandro.Burigana <burigana@inf.unibz.it>
2023-05-10 11:42:58 +0200

    Adding skeleton for parser generator (to finish)

--- include/generators/parser/parser_gen.h
+++ include/generators/parser/parser_gen.h
@@ -16,6 +16,22 @@ namespace epddl {
 #undef ast_class
 #undef function_name
 #undef class_name
+
+#define class_name(c_name) c_name
+#define function_name(arg) parse_##arg
+#define ast_class(name, params) \
+    ast::unique_ptr(name) function_name(name)() { \
+        return {};                                \
+    }
+
+#define epddl_element(ast_class, _) ast_class
+    // Prototypes of parsing functions
+    epddl_all_element_defs
+
+#undef epddl_element
+#undef ast_class
+#undef function_name
+#undef class_name
 }
 
 #endif //EPDDL_PARSER_GEN_H
