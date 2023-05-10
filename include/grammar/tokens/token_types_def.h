1e63ff77cd1947c4ea29c3595baf5c6acafed46f
Alessandro.Burigana <burigana@inf.unibz.it>
2023-05-10 11:40:04 +0200

    Renaming basic tokens to 'pattern tokens'

--- include/grammar/tokens/token_types_def.h
+++ include/grammar/tokens/token_types_def.h
@@ -3,7 +3,7 @@
 
 #define epddl_special_token_type         epddl_token_type(special_token)
 #define epddl_punctuation_token_type     epddl_token_type(punctuation_token)
-#define epddl_basic_token_type           epddl_token_type(basic_token)
+#define epddl_pattern_token_type         epddl_token_type(pattern_token)
 #define epddl_keyword_token_type         epddl_token_type(keyword_token)
 #define epddl_connective_token_type      epddl_token_type(connective_token)
 #define epddl_quantifier_token_type      epddl_token_type(quantifier_token)
@@ -38,9 +38,9 @@
         )                             \
     )
 
-#define epddl_basic_tokens_def \
+#define epddl_pattern_tokens_def  \
     epddl_tokens(                 \
-        epddl_basic_token_type,   \
+        epddl_pattern_token_type, \
         tokens(                   \
             epddl_tok_identifier  \
             epddl_tok_variable    \
@@ -162,7 +162,7 @@
 #define epddl_all_token_types_def \
     epddl_special_tokens_def         \
     epddl_punctuation_tokens_def     \
-    epddl_basic_tokens_def           \
+    epddl_pattern_tokens_def           \
     epddl_keyword_tokens_def         \
     epddl_connective_tokens_def      \
     epddl_quantifier_tokens_def      \
