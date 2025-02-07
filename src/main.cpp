cefa95989d56dfd74546c0f6b5538327df3f8255
Alessandro.Burigana <burigana@inf.unibz.it>
2025-02-07 14:34:18 +0100

    Simplified main

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,9 @@
 #include "../include/parser/parser_helper.h"
-#include <string>
 
 using namespace epddl;
 
-int main() {
-//    std::string path = "../examples/libraries/ma_star.epddl";
-//    std::string path = "../examples/domains/Coin-in-the-Box/cb.epddl";
-//    std::string path = "../examples/domains/Collaboration-and-Communication/cc.epddl";
-//    std::string path = "../examples/domains/Assemble/assemble.epddl";
-    std::string path = "../examples/domains/Coin-in-the-Box/instances/cb2.epddl";
-
-    parser_helper helper{path};
+int main(int argc, char *argv[]) {
+    parser_helper helper{argv[1]};
     helper.parse();
 
     return 0;
